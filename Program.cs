71db2d353d9b2275039e261282dfbd9ed1677046
Darshil1142 <130062191+Darshil1142@users.noreply.github.com>
2023-04-09 00:56:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    await using Stream stream =
+    await client.GetStreamAsync("https://localhost:7289/api/Students");
+    var repositories =
+        await JsonSerializer.DeserializeAsync<List<Name>>(stream);
+   
+
+
+    foreach (var repo in repositories ?? Enumerable.Empty<Name>())
+        Console.WriteLine(repo.name+" ");
+    
+    Console.ReadKey();
+}
